fc91c13e3b324759544cf39c1e95f07db321901c
Manjeera <manjeera.thogarcheti@gmail.com>
2021-07-20 15:37:26 -0400

    Add Expense form added with multiple states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -35,9 +36,8 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
-      
     </div>
   );
 }
